487e71d6222c985ac50b5e52eaba5f87ffcd3d7f
Nawaf <63883911+Nawaf-Fahad@users.noreply.github.com>
2023-02-20 22:06:18 +0300

    Initial Commit

---
+++ CustomTabBar/CustomTabBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomTabBarApp.swift
+//  CustomTabBar
+//
+//  Created by Nawaf Alotaibi on 20/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct CustomTabBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
